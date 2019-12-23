903f5c3a4cd671f5008bdc85c054f7fc5a7d70ae
kato <naka.u926@gmail.com>
2019-12-23 09:06:36 +0900

    Update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,32 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        nuxt_app
-      </h1>
-      <h2 class="subtitle">
-        my nuxt_app for learning
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <DefaultInputForm />
+      <CreateInputForm />
+      <Btn :button="preview_btn"></Btn>
+      <Btn :button="save_btn"></Btn>
+      <Btn :button="comfirm_btn"></Btn>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue"
+import DefaultInputForm from "~/components/organisms/DefaultInputForm.vue"
+import CreateInputForm from "~/components/organisms/CreateInputForm.vue"
+import Btn from "~/components/atoms/Btn.vue"
 
 export default {
   components: {
-    Logo
+    DefaultInputForm,
+    CreateInputForm,
+    Btn
+  },
+  data() {
+    return {
+      preview_btn: "プレビュー",
+      save_btn: "一時保存",
+      comfirm_btn: "確認画面へ進む"
+    }
   }
 }
 </script>
@@ -37,32 +34,10 @@ export default {
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
     "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
